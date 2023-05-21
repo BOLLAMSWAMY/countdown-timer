9d38175828eac3a04935be32bc00000a5d9e4d56
BOLLAMSWAMY <swamybolkam@gmail.com>
2023-05-21 16:37:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYear="1 Jan 2024";
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const diff=(newYearDate - currentDate)/1000;
+    
+    const days = Math.floor(diff/3600/24);
+    const hours = Math.floor(diff/3600)%24;
+    const minutes = Math.floor(diff/60)%60;
+    const seconds = Math.floor(diff)%60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+console.log(days, hours, minutes, seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countdown();
+
+setInterval(countdown,1000);
